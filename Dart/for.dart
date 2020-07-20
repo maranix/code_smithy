b3f0160ca56cc770e5c2e50c2f8561dac22231fc
ramanverma-dev <ramanverma4183@gmail.com>
2020-07-20 23:29:04 +0530

    Dart: 'for-in'

--- Dart/for.dart
+++ Dart/for.dart
@@ -2,4 +2,10 @@ void main() {
   for (var i = 0; i < 5; i++) {
     print(i);
   }
+
+  //"for-in" can be used on any iterable classes
+  var sequence = [6, 7, 8];
+  for (var x in sequence) {
+    print(x);
+  }
 }
