c047fc6fe2833c86b62390cf7c3507632efdc14b
ramanverma-dev <ramanverma4183@gmail.com>
2020-07-20 20:59:58 +0530

    Dart: Values

---
+++ Dart/Values.dart
@@ -0,0 +1,13 @@
+void main() => {
+      //String appending
+      print("Val" + "ues"),
+
+      //Integer and Float sum calculation
+      print("1+1=${1 + 1}"),
+      print("15.7+12.5=${15.7 + 12.5}"),
+
+      //Booleans
+      print(true && false),
+      print(false || true),
+      print(!true)
+    };
