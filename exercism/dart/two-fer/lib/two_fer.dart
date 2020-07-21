5380a7846a3924b191eb0b7891b91571db449e8c
ramanverma-dev <ramanverma4183@gmail.com>
2020-07-21 20:01:07 +0530

    Exercism:dart: two-fer

---
+++ exercism/dart/two-fer/lib/two_fer.dart
@@ -0,0 +1,13 @@
+String twoFer() {
+  return ('One for you, one for me.');
+}
+
+/*
+String twoFer(Alice) {
+  return ('One for ${Alice}, one for me.');
+}
+
+String twoFer(Bob) {
+  return ('One for ${Bob}, one for me.');
+}
+*/
